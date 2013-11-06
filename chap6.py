06962a114eb65e8fc132186f3940934843c9afea
sgallaghma <stephen.james.gallagher@gmail.com>
2013-11-05 20:36:14 -0500

    steve sprint 2 submission

--- chap6.py
+++ chap6.py
@@ -1,9 +1,80 @@
 # Enter your answrs for chapter 6 here
-# Name:
+# Name: Steve Gallagher
 
 
 # Ex. 6.6
 
+def first(word, z):
+	return word[z]
+
+def last(word, z):
+	return word[-z]
+
+
+def is_palindrome(word):
+
+	a = 0
+	b = 1
+
+	c = len(word)
+	print "length of word is", c
+	if c % 2 == 0:
+		c = c
+	else:
+		c = c-1
+
+	n = len(word)
+	if n % 2 == 0:
+		n = n
+	else:
+		n = n-1
+	print "initial value for n is", n	
+
+	def checker(word, a, b, c, n):
+			if c/2 > n:
+				print "yes, this word is a palindrome"  
+
+			else:
+				if first(word, a) != last(word, b):
+					print "RECURSE", n
+					print "value for a is", a, "which is", first(word, a)
+					print "value for b is", b, "which is", last(word, b)
+					print "no, this word is not a palindrome"
+				else:
+					print "RECURSE", n
+					print "a is", a, "which is", first(word, a)
+					print "b is", b, "which is", last(word, b)
+					checker(word, (a+1), (b+1), c, (n-1))
+
+
+	checker(word, a, b, c, n)
+
+is_palindrome('redikutgcjhgider')
+
 
 
 # Ex 6.8
+
+
+def gcd(a,b):
+
+	if b == 0:
+		print "The greatest common divisor is", a
+
+	else:
+		r = a % b
+		a = b
+		b = r
+
+		gcd(a,b)
+
+
+
+#gcd(100,12)
+
+
+
+
+
+
+
--- chap7.py
+++ chap7.py
@@ -1,9 +1,46 @@
 # Enter your answrs for chapter 7 here
-# Name:
+# Name: Stephen Gallagher
+
+
+from __future__ import division
+
+
+import math
 
 
 # Ex. 7.5
 
 
+def estimate_pi():
 
-# How many iterations does it take to converge?
+	k = 0
+
+	term = 0
+
+	x = 2*math.sqrt(2)/9801
+
+	def iterate(k, x, term):
+
+		print "ITERATION"
+		
+		a = math.factorial(4*k)*(1103+(26390*k))
+
+		b = (math.factorial(k)**4)*(396**(4*k))
+
+		term =  term + a/b
+		
+		if a/b < 1e-15:
+			print "function estimate of pi is", 1/(x * term)
+
+		else: 
+
+			k = k + 1
+
+			iterate(k, x, term)	
+		
+	iterate(k, x, term)
+	
+estimate_pi()
+
+
+# How many iterations does it take to converge?    4 Iterations
